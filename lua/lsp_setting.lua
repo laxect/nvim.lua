c706c1e3a3301a28e4a5a772db8564030a4eed54
gyara <me@gyara.moe>
2021-03-27 12:58:37 +0900

    typescript-language-server

--- lua/lsp_setting.lua
+++ lua/lsp_setting.lua
@@ -13,7 +13,7 @@ vim.api.nvim_exec([[
     sign define LspDiagnosticsSignHint text=🠒 texthl=LspDiagnosticsDefaultHint linehl= numhl=  
 ]], true)
 
-local servers = { "lua", "rust", "haskell" }
+local servers = { "lua", "rust", "haskell", "js" }
 for _, lsp in ipairs(servers) do
   require ("lang."..lsp)
 end
