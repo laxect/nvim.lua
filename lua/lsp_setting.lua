8492a2bd72d527b19e19704089a743d97e76a769
gyara <me@gyara.moe>
2021-05-24 13:27:23 +0900

    gdscript lang server

--- lua/lsp_setting.lua
+++ lua/lsp_setting.lua
@@ -13,7 +13,7 @@ vim.api.nvim_exec([[
     sign define LspDiagnosticsSignHint text=🠒 texthl=LspDiagnosticsDefaultHint linehl= numhl=  
 ]], true)
 
-local servers = { "lua", "rust", "haskell", "js", "bash" }
+local servers = { "lua", "rust", "haskell", "js", "bash", "gdscript" }
 for _, lsp in ipairs(servers) do
   require ("lang."..lsp)
 end
