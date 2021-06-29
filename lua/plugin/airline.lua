4736e748d29a3f5f8fe9706e110f0a300d7912a5
gyara <me@gyara.moe>
2021-06-29 19:03:14 +0900

    found a amazing theme for airline

--- lua/plugin/airline.lua
+++ lua/plugin/airline.lua
@@ -1,7 +1,7 @@
 local setup = function ()
   local u = require('utils')
   u.g('airline_powerline_fonts', 1)
-  u.g('airline_theme', 'google_light')
+  u.g('airline_theme', 'transparent')
   u.g('airline_extensions', {'tabline', 'branch'})
 end
 
