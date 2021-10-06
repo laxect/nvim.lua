ce087584bb99c38842f46dd3c2a573bdbb31f56a
gyara <me@gyara.moe>
2021-10-06 16:46:25 +0900

    remove tab keymap. use default maps instead.

--- init.lua
+++ init.lua
@@ -50,8 +50,6 @@ require'nvim-treesitter.configs'.setup {
 
 -- # key map
 -- buffer
-u.map('n', '<Leader>j', '<cmd>tabprev<cr>')
-u.map('n', '<Leader>k', '<cmd>tabnext<cr>')
 u.map('n', '<Leader>q', '<cmd>tabclose<cr>')
 -- close highlighter
 u.map('n', '<Leader>n', '<cmd>nohl<cr>')
