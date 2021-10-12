65d38b4219c37857b9b1bbe8bd356af0db9433ca
gyara <me@gyara.moe>
2021-10-12 16:33:32 +0900

    rewrite galaxyline setting

--- lua/plugin/statusline.lua
+++ lua/plugin/statusline.lua
@@ -9,8 +9,8 @@ M.config = function()
 
   local colors = require('colors')
 
-  gls.left[1] = {
-    statusIcon = {
+  table.insert(gls.left, {
+    StatusIcon = {
       provider = function()
         return '   '
       end,
@@ -18,9 +18,9 @@ M.config = function()
       separator = ' ',
       separator_highlight = { colors.blue, colors.black },
     },
-  }
+  })
 
-  gls.left[2] = {
+  table.insert(gls.left, {
     FileName = {
       provider = { 'FileName' },
       condition = condition.buffer_not_empty,
@@ -28,9 +28,9 @@ M.config = function()
       separator = ' ',
       separator_highlight = { colors.black, colors.white },
     },
-  }
+  })
 
-  gls.left[3] = {
+  table.insert(gls.left, {
     Brand = {
       provider = function()
         return '存在するということは、自分を创造することだ。'
@@ -42,17 +42,17 @@ M.config = function()
       separator = ' ',
       separator_highlight = { colors.black, colors.white },
     },
-  }
+  })
 
-  gls.left[4] = {
-    current_dir = {
+  table.insert(gls.left, {
+    CurrentDir = {
       provider = function()
         local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
         return ' ' .. dir_name .. ' '
       end,
       highlight = { colors.black, colors.white },
     },
-  }
+  })
 
   local checkwidth = function()
     local squeeze_width = vim.fn.winwidth(0) / 2
@@ -62,34 +62,34 @@ M.config = function()
     return false
   end
 
-  gls.left[5] = {
+  table.insert(gls.left, {
     DiffAdd = {
       provider = 'DiffAdd',
       condition = checkwidth,
       icon = ' +',
       highlight = { colors.green, colors.white },
     },
-  }
+  })
 
-  gls.left[6] = {
+  table.insert(gls.left, {
     DiffModified = {
       provider = 'DiffModified',
       condition = checkwidth,
       icon = '  *',
       highlight = { colors.cyan, colors.white },
     },
-  }
+  })
 
-  gls.left[7] = {
+  table.insert(gls.left, {
     DiffRemove = {
       provider = 'DiffRemove',
       condition = checkwidth,
       icon = ' -',
       highlight = { colors.yellow, colors.white },
     },
-  }
+  })
 
-  gls.right[1] = {
+  table.insert(gls.right, {
     FileType = {
       provider = function()
         local ft = vim.bo.filetype
@@ -98,23 +98,23 @@ M.config = function()
       condition = condition.buffer_not_empty,
       highlight = { colors.red, colors.white },
     },
-  }
+  })
 
-  gls.right[2] = {
-    lsp_status = {
+  table.insert(gls.right, {
+    LSPStatus = {
       provider = function()
         local clients = vim.lsp.get_active_clients()
         if next(clients) ~= nil then
-          return ' ' .. require('lsp-status').status()
+          return ' ' .. require('lsp-status').status():gsub('%%%%', '%%') -- a temp fix for '%%' in lsp status
         else
           return ''
         end
       end,
       highlight = { colors.black, colors.white },
     },
-  }
+  })
 
-  gls.right[3] = {
+  table.insert(gls.right, {
     GitIcon = {
       provider = function()
         return ' '
@@ -122,26 +122,26 @@ M.config = function()
       condition = condition.check_git_workspace,
       highlight = { colors.red, colors.white },
     },
-  }
+  })
 
-  gls.right[4] = {
+  table.insert(gls.right, {
     GitBranch = {
       provider = 'GitBranch',
       condition = condition.check_git_workspace,
       highlight = { colors.black, colors.white },
     },
-  }
+  })
 
-  gls.right[5] = {
-    some_icon = {
+  table.insert(gls.right, {
+    Sep = {
       provider = function()
         return ' '
       end,
       highlight = { colors.white, colors.red },
     },
-  }
+  })
 
-  gls.right[6] = {
+  table.insert(gls.right, {
     ViMode = {
       provider = function()
         local alias = {
@@ -163,40 +163,34 @@ M.config = function()
       end,
       highlight = { colors.white, colors.red },
     },
-  }
+  })
 
-  gls.right[7] = {
-    some_icon2 = {
+  table.insert(gls.right, {
+    Sep2 = {
       provider = function()
         return ''
       end,
       highlight = { colors.red, colors.green },
     },
-  }
+  })
 
-  gls.right[8] = {
-    line_percentage = {
+  table.insert(gls.right, {
+    LinePercentage = {
       provider = function()
         local current_line = vim.fn.line('.')
-        local total_line = vim.fn.line('$')
-
-        if current_line == 1 then
-          return '  Top '
-        elseif current_line == vim.fn.line('$') then
-          return '  Bot '
-        end
-        local result, _ = math.modf((current_line / total_line) * 100)
-        return '  ' .. result .. '% '
+        local col = vim.fn.col('.')
+        return '  ' .. current_line .. ',' .. col .. ' '
       end,
       highlight = { colors.white, colors.green },
     },
-  }
+  })
 
-  gls.short_line_left[1] = {
-    FileName = {
-      provider = { 'FileName' },
-      condition = condition.buffer_not_empty,
-      highlight = { colors.white, colors.black },
+  gls.short_line_left = {
+    {
+      FileName = {
+        provider = { 'FileName' },
+        highlight = { colors.white, colors.black },
+      },
     },
   }
 end
