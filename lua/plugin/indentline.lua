eb133537d9d76eeabdd6b6d7c08551e11a09f6e2
gyara <me@gyara.moe>
2021-10-12 17:01:25 +0900

    disable some feature

--- lua/plugin/indentline.lua
+++ lua/plugin/indentline.lua
@@ -3,7 +3,7 @@ local M = {}
 M.config = function()
   require('indent_blankline').setup({
     char = '┊',
-    show_current_context = true,
+    -- show_current_context = true,
     show_first_indent_level = false,
     buftype_exclude = { 'terminal', 'help', 'nofile', 'quickfix' },
   })
