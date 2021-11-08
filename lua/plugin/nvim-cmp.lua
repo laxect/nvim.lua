40e534527129d953315f45fe0a7a4efa886d48d1
gyara <me@gyara.moe>
2021-11-08 14:32:48 +0900

    Revert autopair on cmp event.

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -69,6 +69,9 @@ M.config = function()
   })
 
   require('nvim-autopairs').setup({})
+  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  cmp.event:clear()
+  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 end
 
 return M
